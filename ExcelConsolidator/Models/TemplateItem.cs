9c11224cc6c9f83af635bd7bc8adebd28c91e5c2
John van Dijk <john@johnv.ca>
2026-02-28 06:51:04 -0500

    Add project files.

---
+++ ExcelConsolidator/Models/TemplateItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExcelConsolidator.Models
+{
+    internal class TemplateItem
+    {
+        public string SourceSheet { get; set; }         // e.g. Sheet1, EmployeeData
+        public string SourceReference { get; set; }     // e.g. A1, B27
+        public string OutputSheet { get; set; }         // The Sheet where the new table is created. 
+        public int OutputColumn { get; set; }           // Can only be a column number, because it will be the same each row. 
+        public string OutputColumnName { get; set; }    // The name of the output column. 
+
+    }
+}
---
+++ ExcelConsolidator/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Check if any arguments were passed
+        if (args.Length > 0)
+        {
+            Console.WriteLine($"Total arguments: {args.Length}");
+
+            foreach (string arg in args)
+            {
+                Console.WriteLine($"Argument: {arg}");
+            }
+        }
+        else
+        {
+            Console.WriteLine("No arguments were provided.");
+        }
+    }
+}
